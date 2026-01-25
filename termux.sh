d05ac0636b6f1aaf201b7d607c7af1864069083d
xeyuji2ntian2 <xeyuji2ntian2@kolabnow.com>
2026-01-25 22:44:01 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -15,43 +15,44 @@ if [ "$(id -u)" = "0" ]; then
   exit 1
 fi
 
-echo -e "\e[32m======================== Update & upgrade ========================\e[0m\n\n\n\n"
-yes | pkg update && pkg upgrade -y
+echo -e "\n\n\n\n\e[32m======================== Update & upgrade ========================\e[0m\n\n\n\n"
+
+pkg update && pkg upgrade -y
 
 
 
 
-echo -e "\e[32m======================== install make ========================\e[0m\n\n\n\n"  
-yes | pkg install make -y
+echo -e "\n\n\n\n\e[32m======================== install make ========================\e[0m\n\n\n\n"  
+pkg install make -y
 
-echo -e "\e[32m======================== install autoconf ========================\e[0m\n\n\n\n"  
-yes | pkg install autoconf -y 
+echo -e "\n\n\n\n\e[32m======================== install autoconf ========================\e[0m\n\n\n\n"  
+pkg install autoconf -y 
 
-echo -e "\e[32m======================== install automake ========================\e[0m\n\n\n\n"  
-yes | pkg install automake -y
+echo -e "\n\n\n\n\e[32m======================== install automake ========================\e[0m\n\n\n\n"  
+pkg install automake -y
 
-echo -e "\e[32m======================== install libtool ========================\e[0m\n\n\n\n"  
-yes | pkg install libtool -y
+echo -e "\n\n\n\n\e[32m======================== install libtool ========================\e[0m\n\n\n\n"  
+pkg install libtool -y
 
-echo -e "\e[32m======================== install pkg-config========================\e[0m\n\n\n\n"  
-yes | pkg install pkg-config -y
+echo -e "\n\n\n\n\e[32m======================== install pkg-config========================\e[0m\n\n\n\n"  
+pkg install pkg-config -y
 
-echo -e "\e[32m======================== install openssl ========================\e[0m\n\n\n\n"  
-yes | pkg install openssl -y
+echo -e "\n\n\n\n\e[32m======================== install openssl ========================\e[0m\n\n\n\n"  
+pkg install openssl -y
 
-echo -e "\e[32m======================== install libcurl ========================\e[0m\n\n\n\n"  
-yes | pkg install libcurl -y
+echo -e "\n\n\n\n\e[32m======================== install libcurl ========================\e[0m\n\n\n\n"  
+pkg install libcurl -y
 
-echo -e "\e[32m======================== install zlib ========================\e[0m\n\n\n\n"  
-yes | pkg install zlib -y
+echo -e "\n\n\n\n\e[32m======================== install zlib ========================\e[0m\n\n\n\n"  
+pkg install zlib -y
 
-echo -e "\e[32m======================== install git ========================\e[0m\n\n\n\n"  
-yes | pkg install git -y
+echo -e "\n\n\n\n\e[32m======================== install git ========================\e[0m\n\n\n\n"  
+pkg install git -y
 
-echo -e "\e[32m======================== install clang ========================\e[0m\n\n\n\n"  
-yes | pkg install clang -y
+echo -e "\n\n\n\n\e[32m======================== install clang ========================\e[0m\n\n\n\n"  
+pkg install clang -y
 
-echo -e "\e[32m======================== Build CCMINER (CPU ONLY) ========================\e[0m\n\n\n\n"
+echo -e "\n\n\n\n\e[32m======================== Build CCMINER (CPU ONLY) ========================\e[0m\n\n\n\n"
 cd $HOME
 git clone https://github.com/Oink70/ccminer.git
 cd ccminer
