c6aef574196943e106e8b82d869dc277f593000e
xeyuji2ntian2 <xeyuji2ntian2@kolabnow.com>
2026-01-25 23:13:45 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -18,49 +18,18 @@ fi
 echo -e "\n\n\n\n\e[32m======================== Update & upgrade ========================\e[0m\n\n\n\n"
 
 pkg update && pkg upgrade -y -o Dpkg::Options::="--force-confold"
-
-
-
-
-echo -e "\n\n\n\n\e[32m======================== install make ========================\e[0m\n\n\n\n"  
-pkg install make -y
-
-echo -e "\n\n\n\n\e[32m======================== install autoconf ========================\e[0m\n\n\n\n"  
-pkg install autoconf -y 
-
-echo -e "\n\n\n\n\e[32m======================== install automake ========================\e[0m\n\n\n\n"  
-pkg install automake -y
-
-echo -e "\n\n\n\n\e[32m======================== install libtool ========================\e[0m\n\n\n\n"  
-pkg install libtool -y
-
-echo -e "\n\n\n\n\e[32m======================== install pkg-config========================\e[0m\n\n\n\n"  
-pkg install pkg-config -y
-
-echo -e "\n\n\n\n\e[32m======================== install openssl ========================\e[0m\n\n\n\n"  
-pkg install openssl -y
-
-echo -e "\n\n\n\n\e[32m======================== install libcurl ========================\e[0m\n\n\n\n"  
-pkg install libcurl -y
-
-echo -e "\n\n\n\n\e[32m======================== install zlib ========================\e[0m\n\n\n\n"  
-pkg install zlib -y
-
-echo -e "\n\n\n\n\e[32m======================== install git ========================\e[0m\n\n\n\n"  
-pkg install git -y
-
-echo -e "\n\n\n\n\e[32m======================== install clang ========================\e[0m\n\n\n\n"  
-pkg install clang -y
+pkg install dos2unix make autoconf automake libtool pkg-config openssl libcurl zlib git clang -y 
 
 echo -e "\n\n\n\n\e[32m======================== Build CCMINER (CPU ONLY) ========================\e[0m\n\n\n\n"
 cd $HOME
-git clone https://github.com/Oink70/ccminer.git
-cd ccminer
+git clone https://github.com/Oink70/ccminer-verus.git
+cd ccminer-verus
 
 export CC=clang
 export CXX=clang++
-export CFLAGS="-O2"
-export LDFLAGS=""
+export CFLAGS="-O3 -march=native"
+export CXXFLAGS="-O3 -march=native"
+
 
 ./autogen.sh
 
@@ -70,6 +39,7 @@ export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
   --disable-cuda \
   --disable-opencl \
   --with-crypto \
+  --with-curl \
   CPPFLAGS="-I$PREFIX/include" \
   LDFLAGS="-L$PREFIX/lib" \
   --prefix=$PREFIX
