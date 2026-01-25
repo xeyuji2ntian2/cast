b4e0487ccdb756ee6f377de3c8b24ff5e24900a9
xeyuji2ntian2 <xeyuji2ntian2@kolabnow.com>
2026-01-26 01:50:13 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 set -euo pipefail
-dos2unix "$0" 2>/dev/null || true
+
 
 export HOME=/data/data/com.termux/files/home
 export PREFIX=/data/data/com.termux/files/usr
