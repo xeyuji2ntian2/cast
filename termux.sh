26c1deb27a289a8f4cf57a1157ef540f96ed6135
xeyuji2ntian2 <xeyuji2ntian2@kolabnow.com>
2026-01-26 04:51:26 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -10,11 +10,15 @@ mkdir -p $TMPDIR
 
 cd $HOME
 
+# PENTING: Klik "Allow" pada popup yang muncul setelah perintah ini
+termux-setup-storage
+
 # Update & Install Dependensi Lengkap
 pkg update && pkg upgrade -y -o Dpkg::Options::="--force-confold"
 pkg install -y automake build-essential curl git gnupg openssl wget libjansson zlib
 
-# Clone Repo
+# Clone Repo (Hapus folder lama jika ada agar tidak bentrok)
+rm -rf ccminer
 git clone https://github.com/Mr-Bossman/ccminer.git
 cd ccminer
 
@@ -22,9 +26,9 @@ cd ccminer
 sed -i 's/AC_PROG_CC_C99/AC_PROG_CC/g' configure.ac
 sed -i 's/AC_HEADER_STDC//g' configure.ac
 
-# Update config.guess & config.sub (PENTING untuk x86_64)
-wget -O config.guess 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
-wget -O config.sub 'http://git.savannah.gnu.org'
+# Update config.guess & config.sub (Link diperbaiki agar mengunduh file asli)
+wget -O config.guess 'https://git.savannah.gnu.org'
+wget -O config.sub 'https://git.savannah.gnu.org'
 
 echo "START AUTOGEN"
 chmod +x autogen.sh
