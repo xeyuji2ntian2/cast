3460a8e87f271a6b91f3a6772b9f0b818dce670a
xeyuji2ntian2 <xeyuji2ntian2@kolabnow.com>
2026-01-26 04:16:18 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 export HOME=/data/data/com.termux/files/home
 export PREFIX=/data/data/com.termux/files/usr
 export PATH=$PREFIX/bin:$PATH
+export TMPDIR=$PREFIX/tmp
+mkdir -p $TMPDIR
+
 cd $HOME
 echo "=== TERMUX ENV ==="
 whoami
@@ -18,11 +21,6 @@ curl -s https://its-pointless.github.io/setup-pointless-repo.sh | bash
 pkg install -y gcc-6
 
 
-git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
-cd ccminer
-chmod +x build.sh autogen.sh configure.sh
-./autogen.sh
-./configure.sh
-./build.sh
-
+wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer
+chmod +x ccminer
 ./ccminer --help
