2e55a1ddfa8109dc3a5424620487c146dc143456
xeyuji2ntian2 <xeyuji2ntian2@kolabnow.com>
2026-01-26 01:41:11 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -12,3 +12,9 @@ id
 uname -a
 apt update -y 
 apt install libcurl openssl libjansson automake build-essential screen git -y
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
