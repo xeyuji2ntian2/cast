324da1c787d10a88377f39fc984978c56fab626d
xeyuji2ntian2 <xeyuji2ntian2@kolabnow.com>
2026-01-28 00:50:07 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -13,6 +13,7 @@ cd $HOME
 # Update & Install Dependensi Lengkap
 pkg update && pkg upgrade -y -o Dpkg::Options::="--force-confold"
 pkg install -y \
+  libtool \
   automake \
   libjansson \
   libcurl4 \
@@ -20,5 +21,7 @@ pkg install -y \
   libssl \
   build-essential \
   clang binutils \
-  git \
+  git
 git clone https://github.com/JayDDee/cpuminer-opt.git
+cd cpuminer-opt
+./build.sh
